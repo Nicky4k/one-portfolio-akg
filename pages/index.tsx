ba7281b9631c40b88cd0c98ee64b0e7c3428bb56
Nicky <nickysauceofficial@gmail.com>
2023-03-06 23:34:19 +0530

    one portfolio done

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,8 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 ">
       <Head>
-        <title>One Portfolio - AKG</title>
-        <meta name="description" content="One Portfolio, Ashwini Gupta" />
+        <title>Ashwini Gupta - React JS</title>
+        <meta name="description" content="Portfolio, Ashwini Gupta" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
